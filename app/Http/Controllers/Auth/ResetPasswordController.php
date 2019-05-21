f743a062e81a2b8f479543d9c9076095adc9b348
qingfeng1997 <1373823008@qq.com>
2019-05-21 14:51:55 +0000

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -36,4 +37,13 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+
+    protected function sendResetResponse(Request $request, $response)
+    {
+	    session()->flash('success', '密码更新成功，您已成功登录！');
+	    return redirect($this->redirectPath());
+    }
+
+
 }
