d375508715bc3d625f8ffb39856497bd9d6a0b76
qingfeng1997 <1373823008@qq.com>
2019-05-25 15:52:20 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('auth', ['except' => ['show']]);
+	}
 	public function show(User $user)
 	{
 		return view('users.show',compact('user'));
@@ -16,15 +20,17 @@ class UsersController extends Controller
 
 	public function edit(User $user)
 	{
+		$this->authorize('update', $user);
 		return view('users.edit',compact('user'));
 	}
 
 	public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
 	{
+		$this->authorize('update', $user);
 		$data = $request->all();
 
 		if ($request->avatar) {
-			$result = $uploader->save($request->avatar, 'avatars', $user->id,416);
+			$result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
 			if ($result) {
 				$data['avatar'] = $result['path'];
 			}
@@ -32,6 +38,5 @@ class UsersController extends Controller
 
 		$user->update($data);
 		return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
-
 	}
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,24 +7,28 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		// 'App\Model' => 'App\Policies\ModelPolicy',
+	];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+	/**
+	 * Register any authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
+		// 修改策略自动发现的逻辑
+		Gate::guessPolicyNamesUsing(function ($modelClass) {
+			// 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
+			return 'App\Policies\\'.class_basename($modelClass).'Policy';
+		});
 
-        //
-    }
+	}
 }
