c144fb8ac86f981c216ba494b8c778d4bb6b99ef
qingfeng1997 <1373823008@qq.com>
2019-05-27 07:21:47 +0000

    用户话题列表

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -38,11 +38,11 @@ class UsersTableSeeder extends Seeder
         User::insert($user_array);
 
         // 单独处理第一个用户的数据
-#        $user = User::find(1);
-#        $user->name = 'Summer';
-#        $user->email = 'summer@example.com';
-#        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
-#        $user->save();
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
 
     }
 }
