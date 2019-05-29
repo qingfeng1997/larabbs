55c8d9024eaf84801f6d7256045ce5c75b00917e
qingfeng1997 <1373823008@qq.com>
2019-05-29 09:29:21 +0000

    回复数据

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -52,4 +52,9 @@ class Topic extends Model
 	{
 		return route('topics.show', array_merge([$this->id, $this->slug], $params));
 	}
+	//一篇帖子下有多条回复
+	public function replies()
+	{
+		return $this->hasMany(Reply::class);
+	}
 }
