5f292e10f5b0bde088a5f293396f22599c9a0120
qingfeng1997 <1373823008@qq.com>
2019-05-29 11:30:31 +0000

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -4,38 +4,17 @@ namespace App\Http\Requests;
 
 class ReplyRequest extends Request
 {
-    public function rules()
-    {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
-    }
+	public function rules()
+	{
+		return [
+			'content' => 'required|min:2',
+		];
+	}
 
-    public function messages()
-    {
-        return [
-            // Validation messages
-        ];
-    }
+	public function messages()
+	{
+		return [
+			// Validation messages
+		];
+	}
 }
