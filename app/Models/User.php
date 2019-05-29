d9668f1e0744711552325ef0a71f995344275827
qingfeng1997 <1373823008@qq.com>
2019-05-29 11:45:28 +0000

    消息通知有新回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,51 +6,71 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-     use Notifiable, MustVerifyEmailTrait;
+	use  MustVerifyEmailTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password','introduction','avatar',
-    ];
+	use Notifiable{
+		notify as protected laravelNotify;
+	}
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password','introduction','avatar',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    public function topics()
-    {
-	 return $this->hasMany(Topic::class);
-    }
-   //判断useId 是否等于 模型里面的ID
-    public function isAuthorOf($model)
-    {
-         return $this->id == $model->user_id;
-    }
+	public function topics()
+	{
+		return $this->hasMany(Topic::class);
+	}
+	//判断useId 是否等于 模型里面的ID
+	public function isAuthorOf($model)
+	{
+		return $this->id == $model->user_id;
+	}
 
-    public function replies()
-    {
-	 return $this->hasMany(Reply::class);
-    }
+	public function replies()
+	{
+		return $this->hasMany(Reply::class);
+	}
+
+
+	public function notify($instance)
+	{
+		// 如果要通知的人是当前用户，就不必通知了！
+		if ($this->id == Auth::id()) {
+			return;
+		}
+
+		// 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+		if (method_exists($instance, 'toDatabase')) {
+			$this->increment('notification_count');
+		}
+
+		$this->laravelNotify($instance);
+	}
 
 }
